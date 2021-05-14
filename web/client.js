07142e475474c77d1572149c366191c281541575
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2021-05-14 23:24:18 +0200

    last commit plz

--- web/client.js
+++ web/client.js
@@ -5,5 +5,5 @@ export default sanityClient({
   projectId: '7wam4akj', // you can find this in sanity.json
   dataset: 'prod', // or the name you chose in step 1
   useCdn: false, // `false` if you want to ensure fresh data
-  token: 'skq2lsGCD831mOs4NlehpbIVkbTlRejSzHtUf3cJPpvrda2G5KlcKJbw8urqsRBzZyBCCsMOgRAS6uaXF8Nucj3Z4eQUAw6Oo2VH3ftsK24TEegosEBEEMJkHSMQG74RnZA1DGJkiDfLZiRkXWlG1qE0WxPgdNiLJtZUgYMb1VDtjZivZwhy'
+  token: 'skJ06WFYi6E0apRHQuRwdgGr93jVTnHbk9wzw5tFkrAq4pkKgm190kjaCxKks2rua8lbbHwIpgp2dBbbrGgQfa2g0nS88Gmg9lqsbY4eXPFpvhVPvbpSPIUCH7XZBdooTdrZQ69ZBCynfDA8OxtBL6NradlQMHMbVjg1Q1NZaZB6Kf1umaWK'
 })
